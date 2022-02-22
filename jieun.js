f50ec5d9de36d0484cda4b4c3c7d158405344906
Jieunhann <99030269+Jieunhann@users.noreply.github.com>
2022-02-21 19:41:55 -0500

    Add files via upload

---
+++ jieun.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+alert("WELCOME TO JIEUN'S INTERACTION WEBSITE!");
+})
+      $("#one").draggable();
+      $("#one").hover(function(){
+$("#one").show();
+})
+$("#one").click(function(){
+       $("#one").hide();
+       });
+       $("#thick").hover(function(){
+       $("#thyme").append("whatever");
+       });
